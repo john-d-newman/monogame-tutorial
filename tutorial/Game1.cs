571c7c7a470709881b9686a93176f09719f081a9
john-d-newman <johnnewman122333@gmail.com>
2025-06-18 15:59:00 -0500

    Chapter 1-8
    Code for chapters 1-8 of the tutorial

---
+++ tutorial/Game1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
+
+namespace tutorial;
+
+public class Game1 : Core
+{
+    // texture regino that defines the slime sprite in the atlas
+    // Defines the slime sprite.
+    private Sprite _slime;
+
+    // Defines the bat sprite.
+    private Sprite _bat;
+
+    // The MonoGame logo texture
+    private Texture2D _logo;
+    public Game1() : base("Dungeon Slime", 1280, 720, false)
+    {
+
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/atlas-definition.xml");
+
+        // Create the slime sprite from the atlas.
+        _slime = atlas.CreateSprite("slime");
+        _slime.Scale = new Vector2(4.0f, 4.0f);
+        _slime.Color = Color.Red;
+
+        // Create the bat sprite from the atlas.
+        _bat = atlas.CreateSprite("bat");
+        _bat.Scale = new Vector2(8.0f, 4.0f);
+        _bat.Effects = SpriteEffects.FlipVertically | SpriteEffects.FlipHorizontally;
+        // TODO: use this.Content to load your game content here
+        _logo = Content.Load<Texture2D>("images/logo");
+
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+        // Begin the sprite batch to prepare for rendering.
+        //SpriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        // Draw the slime sprite.
+        _slime.Draw(SpriteBatch, Vector2.One);
+
+        // Draw the bat sprite 10px to the right of the slime.
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+
+        
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}

5e908368ca0258b21634cdc21386aa4eccd2ebcf
john-d-newman <johnnewman122333@gmail.com>
2025-06-18 19:22:37 -0500

    Chapter 10

--- MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
+++ MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+571c7c7a470709881b9686a93176f09719f081a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e26626d57c4f1c48d09e29979cb88841cd2127")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tutorial/obj/Debug/net8.0/tutorial.AssemblyInfo.cs
+++ tutorial/obj/Debug/net8.0/tutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+571c7c7a470709881b9686a93176f09719f081a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e26626d57c4f1c48d09e29979cb88841cd2127")]
 [assembly: System.Reflection.AssemblyProductAttribute("tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
